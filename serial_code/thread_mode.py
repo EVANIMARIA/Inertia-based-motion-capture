ce5a015f5b8d96c5b23db8d01838b952fb275b8b
EVANIMARIA <evanimaria@163.com>
2019-04-03 19:39:31 +0800

    forget_to_push_win_0403

--- serial_code/thread_mode.py
+++ serial_code/thread_mode.py
@@ -1,15 +1,25 @@
-import threading,time
-import serial_mode
+import serial
+import struct
+import threading
+import time
+
+lock = threading.Lock()
 
 
+def serial_mode(sensor, count_out, order):
+    #.acquire()
+    count = 3
+    for count_index in range(count_out):
+    	time.sleep(0.2)
+    	for count_index_in in range(count):
+            print(order)
+    print("done!")
+    #lock.release()
 
-thread_list = []
-t1 = threading.Thread(target=serial_mode, args=("/dev/ttyUSB0",1,9))
-t2 = threading.Thread(target=serial_mode, args=("/dev/ttyUSB1",2,9))
-t1.setDaemon(True)
-t2.setDaemon(True)
-t1.start()
+
+t1 = threading.Thread(target=serial_mode, args=("/dev/ttyUSB0", 3, 1))
+t2 = threading.Thread(target=serial_mode, args=("/dev/ttyUSB1", 3, 2))
 t2.start()
+t1.start()
 t1.join()
 t2.join()
-
