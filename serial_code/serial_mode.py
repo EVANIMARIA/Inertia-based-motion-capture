0fbe539414a4d3d094b324d76d6260667d246a16
evanimaria <evanimaria@163.com>
2019-04-09 22:07:15 +0800

    collecting_Data_0409

--- serial_code/serial_mode.py
+++ serial_code/serial_mode.py
@@ -105,8 +105,8 @@ def serial_mode(sensor, count_out, order):
     # lock.release()
 
 
-t1 = threading.Thread(target=serial_mode, args=("com12", 3, 1))
-t2 = threading.Thread(target=serial_mode, args=("com15", 3, 2))
+t1 = threading.Thread(target=serial_mode, args=("/dev/ttyUSB0", 6, 1))
+t2 = threading.Thread(target=serial_mode, args=("/dev/ttyUSB1", 6, 2))
 # t1.setDaemon(True)
 # t2.setDaemon(True)
 t1.start()
