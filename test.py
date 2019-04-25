901cf47c13216ad39c36b27ed9380e9e8b843eaa
EVANIMARIA <evanimaria@163.com>
2019-04-26 06:23:13 +0800

    svm_on_py_0426

---
+++ test.py
@@ -0,0 +1,50 @@
+import numpy as np
+import os
+from collections import deque
+from sklearn import svm
+
+path1 = 'filter_code/dataxy'
+path2 = 'filter_code/dataxy2/'
+
+dataxy1 = []
+dataxy2 = []
+
+for dirpath, dirnames, filenames in os.walk(path1):
+    for file in filenames:
+        fullpath1 = os.path.join(dirpath, file)
+        with open(fullpath1) as fp1:
+            dataxy_n = fp1.read().split()
+            dataxy_n_iter_list = deque(maxlen=590)
+            for dataxy_n_iter in dataxy_n:
+                dataxy_n_iter_list.append(float(dataxy_n_iter))
+            # dataxy_n = list(filter(None,dataxy_n.split(' ')))
+            # dataxy1 = np.append(dataxy1, dataxy_n_iter_list, axis=0)
+            # dataxy1 = np.reshape(dataxy1, (20, -1))
+            dataxy1.append(dataxy_n_iter_list)
+dataxy1 = np.array(dataxy1)
+print(dataxy1.shape)
+
+for dirpath, dirnames, filenames in os.walk(path1):
+    for file in filenames:
+        fullpath2 = os.path.join(dirpath, file)
+        with open(fullpath2) as fp2:
+            dataxy_n = fp2.read().split()
+            dataxy_n_iter_list = deque(maxlen=590)
+            for dataxy_n_iter in dataxy_n:
+                dataxy_n_iter_list.append(float(dataxy_n_iter))
+            dataxy2.append(dataxy_n_iter_list)
+dataxy2 = np.array(dataxy2)
+print(dataxy2.shape)
+
+train1 = dataxy1[10:17, :]
+train2 = dataxy2[10:17, :]
+train = np.append(train1, train2, axis=0)
+group = np.array([1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
+test = np.append(dataxy1[17:, :], dataxy2[17:, :], axis=0)
+print(test)
+
+clf = svm.SVC(C=0.8, kernel='rbf', gamma=9, decision_function_shape='ovr')
+clf.fit(train, group)
+result = clf.predict(test)
+print(result)
