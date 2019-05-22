f993ab18483e4ff36e3151dd25f3d678d79322b2
evanimaria <evanimaria@163.com>
2019-05-22 20:48:16 +0800

    0522

--- serial_code/th_test.py
+++ serial_code/th_test.py
@@ -15,21 +15,22 @@ def dist(args):
 
 if __name__ == '__main__':
     stamp = str(random.randint(1000,9999))
-    ser1 = serial.Serial("/dev/ttyUSB2", 115200, timeout=100)
+    ser1 = serial.Serial("/dev/ttyUSB0", 115200, timeout=100)
     ser2 = serial.Serial("/dev/ttyUSB1", 115200, timeout=100)
-    ser3 = serial.Serial("/dev/ttyUSB4", 115200, timeout=100)
+    ser3 = serial.Serial("/dev/ttyUSB2", 115200, timeout=100)
+    ser4 = serial.Serial("/dev/ttyUSB3", 115200, timeout=100)
     pool = ThreadPool(3)
     # result = pool.map(serial_mode,[("/dev/ttyUSB3", 6,1,stamp),("/dev/ttyUSB6", 6, 3,stamp),("/dev/ttyUSB5", 6, 4,stamp)])
     # result = pool.map(m_add,[(1,2),(3,4),(5,6)])
     scala_set = deque(maxlen=20)
     sensor_avr = [0,0,0]
-    flag = ['wait','wait','wait']
+    flag = ['wait','wait','wait','wait']
     while True:
-        result = pool.map(serial_mode,[(ser1, 3,1,stamp),(ser2, 3, 3,stamp),(ser3, 3, 4,stamp)])
+        result = pool.map(serial_mode,[(ser1, 3,1,stamp),(ser2, 3, 3,stamp),(ser3, 3, 4,stamp),(ser4, 3, 2,stamp)])
 
         sensor_scala = []
 
-        for i in range(3):
+        for i in range(4):
             sensor_data = result[i][0][0]
             sensor_scala_temp = 0
             for k,v in sensor_data.items():
@@ -40,7 +41,7 @@ if __name__ == '__main__':
         sensor_dist_origin = [0,0,0]
         if len(scala_set) in range(2,21):
             sensor_avr_temp = []
-            for m in range(3):
+            for m in range(4):
                 avr_temp = 0
                 for n in range(len(scala_set)):
                     avr_temp += scala_set[n][m]
@@ -54,6 +55,6 @@ if __name__ == '__main__':
             else:
                 flag[f] = 'wait'
 
-        print('\r'+'a1:'+str(flag[0])+' a2:'+str(flag[1])+' a3:'+str(flag[2]),end = '',flush=True)
+        print('\r'+'a1:'+str(flag[0])+' a2:'+str(flag[1])+' a3:'+str(flag[2])+' a4:'+str(flag[3]),end = '',flush=True)
     pool.close()
     pool.join()
