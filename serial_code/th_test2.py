7743cf6d27a7589d2bb1417bd4bb654c07bd83d6
evanimaria <evanimaria@163.com>
2019-06-20 22:43:00 +0800

    final

--- serial_code/th_test2.py
+++ serial_code/th_test2.py
@@ -28,37 +28,37 @@ if __name__ == '__main__':
     sensor_avr = [0, 0, 0, 0, 0]
     # flag = ['wait','wait','wait','wait']
     while True:
-        #m_input = int(input("please input a:"))
+        m_input = int(input("please input a:"))
         print("wai")
-        # while m_input == 1:
-        print("nei")
-        result = pool.map(serial_mode, [(ser1, 35, 1, stamp), (ser2, 35, 2, stamp), (
-            ser3, 35, 3, stamp), (ser4, 35, 4, stamp), (ser5, 35, 5, stamp)])
-        # result contains of [different process][different sensor][dict of each time sampling]
-        model1 = joblib.load("../test1.m")
-        sensor_scala = []
-        # temp = deque(2)
-        output_mean = []
-        for i in range(5):
-            output = []
+        while m_input == 1:
+            print("nei")
+            result = pool.map(serial_mode, [(ser1, 90, 1, stamp), (ser2, 90, 2, stamp), (
+                ser3, 90, 3, stamp), (ser4, 90, 4, stamp), (ser5, 90, 5, stamp)])
+            # result contains of [different process][different sensor][dict of each time sampling]
+            model1 = joblib.load("../test1.m")
+            sensor_scala = []
+            # temp = deque(2)
+            output_mean = []
+            for i in range(5):
+                output = []
 
-            sensor_data = result[i][0]
-            sensor_arr = np.array(sensor_data)
-            for v in sensor_data:
-                output.append(v['raw_data_ac_x'])
-            output = np.array(output[:380])
-            # output = output.astype('float64')
-            print(str(output.shape) + "   " + str(i))
-            output_mean_single = np.mean(output)
-            output_mean.append(output_mean_single)
-            # print(output_mean)
+                sensor_data = result[i][0]
+                sensor_arr = np.array(sensor_data)
+                for v in sensor_data:
+                    output.append(v['raw_data_ac_x'])
+                output = np.array(output[:380])
+                # output = output.astype('float64')
+                print(str(output.shape) + "   " + str(i))
+                output_mean_single = np.mean(output)
+                output_mean.append(output_mean_single)
+                # print(output_mean)
 
-        # temp.append(output)
-        output_mean = np.array(output_mean)
-        print(output_mean)
-        predicted = model1.predict(output_mean.reshape(-1, 5))
-        print(predicted)
-        m_input = 0
+            # temp.append(output)
+            output_mean = np.array(output_mean)
+            print(output_mean)
+            predicted = model1.predict(output_mean.reshape(-1, 5))
+            print(predicted)
+            m_input = 0
         # print('\r'+'a1:'+str(flag[0])+' a2:'+str(flag[1])+' a3:'+str(flag[2])+' a4:'+str(flag[3])+' a5:'+str(flag[4]),end = '',flush=True)
     pool.close()
     pool.join()
